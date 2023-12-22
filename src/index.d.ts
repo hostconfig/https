6875ccbfd8b48bef8461d73e25ad4e485d922c46
StoneyDSP <nathanjhood@googlemail.com>
2023-12-22 02:16:12 +0100

    Prepping typescript rebuild of https server
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import https from 'https'
-import { IncomingMessage, ServerResponse } from 'node:http'
-
-declare module '@hostconfig/https' {
-  const server: https.Server<typeof IncomingMessage, typeof ServerResponse>
-}
